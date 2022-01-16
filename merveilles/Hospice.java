12e3da8588223ffdc285301e8479e650c236e4b3
Joglodo <72848181+Joglodo@users.noreply.github.com>
2022-01-17 02:23:18 +0400

    Ajout constructeur

---
+++ merveilles/Hospice.java
@@ -0,0 +1,12 @@
+import modele.quartier;
+
+
+public class Hospice extends Quartier{
+  public Hospice(){
+    super("Hospice","MERVEILLE",4,"Si vous n’avez aucune pièce d’or dans votre trésor à la fin de votre tour, gagnez 1 pièce d’or. Si la Sorcière ne reprend pas son tour de jeu, elle ne le termine donc pas et ne peut pas utiliser l’effet de l’Hospice à la fin");
+  }
+  
+  
+  
+  
+}  
