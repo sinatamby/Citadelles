cd6c788b5c7fc7040558a5e064a77d62f404a80f
Bryan <46950203+sinatamby@users.noreply.github.com>
2022-01-16 22:18:53 +0100

    Update Basilique.java

--- merveilles/Basilique.java
+++ merveilles/Basilique.java
@@ -1,1 +1,12 @@
 import modele.quartier;
+
+
+public class Basilique extends Quartier{
+  public Basilique(){
+    super("basilique","MERVEILLE",4,"A la fin de la partie, marquez 1 point suppl ́ementaire pour chaque quartier au cout deconstruction impair dans votre cite");
+  }
+  
+  
+  
+  
+}  
