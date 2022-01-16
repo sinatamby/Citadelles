bfee1cab3ce0e7e93ec3353bed22a78a4bdffdcd
Joglodo <72848181+Joglodo@users.noreply.github.com>
2022-01-17 02:15:03 +0400

    Ajout constructeur

---
+++ merveilles/Observatoire.java
@@ -0,0 +1,12 @@
+import modele.quartier;
+
+
+public class Observatoire extends Quartier{
+  public Observatoire(){
+    super("Observatoire","MERVEILLE",4,"Si vous choisissez de piocher des cartes au d´ebut de votre tour, vous choisissez la carte parmi 3 cartes au lieu de 2.");
+  }
+  
+  
+  
+  
+}  
