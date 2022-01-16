963e48dc448741cad5350151ccee8d5b94b2367d
Joglodo <72848181+Joglodo@users.noreply.github.com>
2022-01-17 02:03:34 +0400

    Ajout constructeur Tresor Impérial

---
+++ merveilles/Tresor_Imperial.java
@@ -0,0 +1,12 @@
+import modele.quartier;
+
+
+public class Tresor_Imperial extends Quartier{
+  public Tresor_Imperial(){
+    super("Tresor Imperial","MERVEILLE",5,"A la fin de la partie, marquez 1 point supplémentaire par pièce d’or dans votre trésor.");
+  }
+  
+  
+  
+  
+}  
