12eb55ae3a26c816f751a578d6c8a39b4dc322a3
Joglodo <72848181+Joglodo@users.noreply.github.com>
2022-01-17 02:30:25 +0400

    Ajout Catacombes

---
+++ merveilles/Catacombes.java
@@ -0,0 +1,13 @@
+import modele.quartier;
+
+
+public class Catacombes extends Quartier{
+  public Catacombes(){
+    super("Catacombes","MERVEILLE",0,"Les catacombes ne peuvent pas être bâties. A la fin de la partie, si vous avez les catacombes  dans votre main, marquez 3 points supplémentaires.
+");
+  }
+  
+  
+  
+  
+}  
