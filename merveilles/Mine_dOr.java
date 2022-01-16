5b3966a9ca6fc6e104465af99df68a6530ab361c
Joglodo <72848181+Joglodo@users.noreply.github.com>
2022-01-17 02:21:44 +0400

    Ajout Constructeur

---
+++ merveilles/Mine_dOr.java
@@ -0,0 +1,12 @@
+import modele.quartier;
+
+
+public class Mine_dOr extends Quartier{
+  public Mine_dOr(){
+    super("Mine d'Or","MERVEILLE",6,"Si vous choisissez de recevoir des pièces d’or en début de tour, prenez-en 1 supplémentaire.");
+  }
+  
+  
+  
+  
+}  
