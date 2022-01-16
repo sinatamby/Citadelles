82be5b424a426d4dc35bb1948b35ddf47d9144cb
Bryan <46950203+sinatamby@users.noreply.github.com>
2022-01-16 21:53:20 +0100

    com

--- extention_Reseau/ServiceClientI.java
+++ extention_Reseau/ServiceClientI.java
@@ -20,7 +20,7 @@ public class ServiceClientI  implements Runnable{
 	private String c_ip;
 	private int c_port;
 
-	private  void terminer(){
+	private  void terminer(){//ferme les connections
 		try{
 			if (ma_connection != null)      
 			{System.out.format("Terminaison pour T_%s\n", id); ma_connection.close();} 
@@ -32,15 +32,15 @@ public class ServiceClientI  implements Runnable{
 		return;
 	}
 
-	public ServiceClientI(Socket la_connection, String mid){
+	public ServiceClientI(Socket la_connection, String mid){//initialise la connexion
 		this.ma_connection= la_connection;
 		this.id=mid;
 		System.out.format("Thread T_%s créé pour traiter la connection\n",id); 
 
 		try{ 
 			InputStreamReader isr = new InputStreamReader(ma_connection.getInputStream(), "UTF-8");
-			input = new BufferedReader(isr) ; 
-			output = new PrintWriter(ma_connection.getOutputStream() , true);
+			input = new BufferedReader(isr) ;//variable qui reçois les messages
+			output = new PrintWriter(ma_connection.getOutputStream() , true);//variable qui envoye les messages
 
 			String c_ip = ma_connection.getInetAddress().toString() ;
 			int c_port= ma_connection.getPort();
@@ -51,7 +51,7 @@ public class ServiceClientI  implements Runnable{
 
 	}
 
-	public  void run(){
+	public  void run(){//methode principale
 		String  message_lu = new String();
 		while (  true  ) {	
 			if(Thread.interrupted()){
@@ -75,30 +75,30 @@ public class ServiceClientI  implements Runnable{
 
 	}
 
-	public void send(String message) {
+	public void send(String message) {//envoye message au client
 		System.out.println("Envoye message sur client"+ id+ ": "+message);
 		this.output.format("%s \n",message);
 	}
 
 	
-	public String read() throws IOException {
+	public String read() throws IOException {//li les messages reçu
 		String message_lu = input.readLine();
 		return(message_lu);
 	}
 
-	public void sleep() {
+	public void sleep() {//envoye sleep au client afin qu'il se mette en veille
 		this.send("SLEEP");
 	}
 	
-	public void wakeUp() {
+	public void wakeUp() {//envoye wakeup au client afin qu'il sorte de la veille
 		this.send("WAKEUP");
 	}
 	
-	public void say(String message) {
+	public void say(String message) {//envoye un message pour que le client l'affiche
 		this.send("SAY:"+message);
 	}
 	
-	public String input(String ask) {
+	public String input(String ask) {//envoye un message au client et attend qu'il retourne une valeur
 		this.send("INPUT:"+ask);
 		System.out.println("marqueur");
 		String tmp = null;
