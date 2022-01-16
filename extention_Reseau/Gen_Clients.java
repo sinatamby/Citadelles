1b8b88999d61abd4338eefdff0957ede297ca2b5
Bryan <46950203+sinatamby@users.noreply.github.com>
2022-01-16 21:42:07 +0100

    ajout com

--- extention_Reseau/Gen_Clients.java
+++ extention_Reseau/Gen_Clients.java
@@ -3,11 +3,11 @@ package extention_Reseau;
 public class Gen_Clients {
 
 	public static void main(String[] args) {
-		String mon_id = String.format("Client_%d", 1);
-		Thread_Client currrent_client = new Thread_Client(mon_id);
+		String mon_id = String.format("Client_%d", 1);//on crée un ID
+		Thread_Client currrent_client = new Thread_Client(mon_id);//on crée un thread client
 		Thread myThread= new Thread(currrent_client);
-		myThread.start();
-		currrent_client.send("je suis le client tu me reçois ?");
+		myThread.start();//on lance Thread
+		currrent_client.send("je suis le client tu me reçois ?");//on envoye au serveur un message textuel
 	}
 
 }
